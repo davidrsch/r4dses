5dfb5fdcf162104f55ea65f4305d6ca10d3a3d4b
davidrsch <daviddrsch@gmail.com>
2024-05-19 17:56:20 +0200

    updating _common.R file

--- _common.R
+++ _common.R
@@ -28,18 +28,18 @@ ggplot2::theme_set(ggplot2::theme_gray(12))
 # use results: "asis" when setting a status for a chapter
 status <- function(type) {
   status <- switch(type,
-    polishing = "should be readable but is currently undergoing final polishing",
-    restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
-    drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
-    complete = "is largely complete and just needs final proof reading",
-    stop("Invalid `type`", call. = FALSE)
+                   polishing = "should be readable but is currently undergoing final polishing",
+                   restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
+                   drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
+                   complete = "is largely complete and just needs final proof reading",
+                   stop("Invalid `type`", call. = FALSE)
   )
 
   class <- switch(type,
-    polishing = "note",
-    restructuring = "important",
-    drafting = "important",
-    complete = "note"
+                  polishing = "note",
+                  restructuring = "important",
+                  drafting = "important",
+                  complete = "note"
   )
 
   cat(paste0(
