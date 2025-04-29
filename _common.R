72909675d554a0c7dae00a7f25b1f53adec0405e
davidrsch <daviddrsch@gmail.com>
2025-04-29 11:01:16 +0200

    Updating to actual state of original version

--- _common.R
+++ _common.R
@@ -6,7 +6,7 @@ knitr::opts_chunk$set(
   # cache = TRUE,
   fig.retina = 2,
   fig.width = 6,
-  fig.asp = 2/3,
+  fig.asp = 2 / 3,
   fig.show = "hold"
 )
 
@@ -27,27 +27,33 @@ ggplot2::theme_set(ggplot2::theme_gray(12))
 
 # use results: "asis" when setting a status for a chapter
 status <- function(type) {
-  status <- switch(type,
-                   polishing = "should be readable but is currently undergoing final polishing",
-                   restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
-                   drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
-                   complete = "is largely complete and just needs final proof reading",
-                   stop("Invalid `type`", call. = FALSE)
+  status <- switch(
+    type,
+    polishing = "should be readable but is currently undergoing final polishing",
+    restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
+    drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
+    complete = "is largely complete and just needs final proof reading",
+    stop("Invalid `type`", call. = FALSE)
   )
 
-  class <- switch(type,
-                  polishing = "note",
-                  restructuring = "important",
-                  drafting = "important",
-                  complete = "note"
+  class <- switch(
+    type,
+    polishing = "note",
+    restructuring = "important",
+    drafting = "important",
+    complete = "note"
   )
 
   cat(paste0(
     "\n",
     ":::: status\n",
-    "::: callout-", class, " \n",
+    "::: callout-",
+    class,
+    " \n",
     "You are reading the work-in-progress second edition of R for Data Science. ",
-    "This chapter ", status, ". ",
+    "This chapter ",
+    status,
+    ". ",
     "You can find the complete first edition at <https://r4ds.had.co.nz>.\n",
     ":::\n",
     "::::\n"
